0c220b3d64f5d737685ba4524040389a452483f5
LesPrimus <amerigo.armentano@hotmail.it>
2025-10-14 15:02:49 +0200

    Remove debugging comments from main.py

--- app/main.py
+++ app/main.py
@@ -3,11 +3,6 @@ import os
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!", file=sys.stderr)
-
-    # Uncomment this block to pass the first stage
-
     command = sys.argv[1]
     if command == "init":
         os.mkdir(".git")
